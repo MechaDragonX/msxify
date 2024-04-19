70403fc48e06702af1cb732f688d52f3e4e4c5b7
MechaDragonX <theofficialrv@gmail.com>
2024-04-18 22:14:19 -0700

    Implement (han)dakuten support

--- msxify.py
+++ msxify.py
@@ -1,11 +1,44 @@
 import sys
 
+diacritics = {
+    'が':  'か\\r゛',
+    'ぎ':  'き\\r゛',
+    'ぐ':  'く\\r゛',
+    'げ':  'け\\r゛',
+    'ご':  'こ\\r゛',
+    'ざ':  'さ\\r゛',
+    'じ':  'し\\r゛',
+    'ず':  'す\\r゛',
+    'ぜ':  'せ\\r゛',
+    'ぞ':  'そ\\r゛',
+    'だ':  'た\\r゛',
+    'ぢ':  'ち\\r゛',
+    'づ':  'つ\\r゛',
+    'で':  'て\\r゛',
+    'ど':  'と\\r゛',
+    'ば':  'は\\r゛',
+    'び':  'ひ\\r゛',
+    'ぶ':  'ふ\\r゛',
+    'べ':  'へ\\r゛',
+    'ぼ':  'ほ\\r゜',
+    'ぱ':  'は\\r゜',
+    'ぴ':  'ひ\\r゜',
+    'ぷ':  'ふ\\r゜',
+    'ぺ':  'へ\\r゜',
+    'ぽ':  'ほ\\r゜',
+}
+
 input = sys.argv[1]
 output = ''
+
 i = 0
 while i < len(input):
-    output += str(input[i])
+    if input[i] in diacritics.keys():
+        output += diacritics[input[i]]
+    else:
+        output += input[i]
     if i != len(input) - 1:
-        output += str('\\r')
+        output += '\\r'
     i += 1
+
 print(output)
